7c0a3dc2a189ab5295795f0bffa6820f99a45227
Chewbaccaun <86445165+Chewbaccaun@users.noreply.github.com>
2022-06-06 15:03:20 -0300

    Update game-main.c

--- game-main.c
+++ game-main.c
@@ -188,10 +188,10 @@ void verPalavra(int tentativas)
 
     for(cont = 0; cont < 5; cont++)
     {
-        for(cont2 = 0; cont2 < 5; cont2++)
+        for(cont2 = 4; cont2 > -1; cont2--)
         {  
-            if(menu[tentativas][cont] == menu[0][cont2] && a[tentativas][cont2] != 1)
-            a[tentativas][cont] = 2;
+            if(menu[tentativas][cont] == menu[0][cont2])
+            a[tentativas][cont2] = 2;
         }
     }
 
